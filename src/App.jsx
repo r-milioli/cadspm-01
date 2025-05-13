6b917f56d2a2e8882c77429d32721cbc4a1e29ec
r-milioli <robson.milioli@gmail.com>
2025-05-13 15:40:44 -0300

    Meu Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+
+import './App.css'
+
+function App() {  
+
+  return (
+    <>
+      <header>
+        <h1>Robson Milioli</h1>
+        <p>Aluno do Curso ADS da PM</p>
+      </header>
+      <main>
+        <h2>Sobre mim</h2>
+        <p>Lorem ipsum dolor sit, amet <strong> adipisicing elit.</strong> Minus quasi 
+          illo sapiente vero voluptatibus nulla a amet sit distinctio 
+          itaque, veritatis quibusdam. Officiis error delectus dicta non totam, odio molestias.</p>
+          <p>Lorem ipsum dolor sit, amet <strong> adipisicing elit.</strong> Minus quasi 
+          illo sapiente vero voluptatibus nulla a amet sit distinctio 
+          itaque, veritatis quibusdam. Officiis error delectus dicta non totam, odio molestias.</p>
+          <h3>Habilidades Principais</h3>
+          <ul>
+            <li>Montagem e configuraçãoes de Pcs</li>
+            <li>Redes e conectividades</li>
+            <li>Sistema operacionais(Windowns, Linux, MS-DOS)</li>
+            <li>Didática Técnicas</li>
+          </ul>
+      </main>
+      <footer>
+            <p>® <strong>Robson Milioli</strong> - Todos os direitos reservados</p>
+      </footer>
+    </>
+  )
+}
+
+export default App
